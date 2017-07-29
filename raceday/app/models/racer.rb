30f7590eb790ea82c9261ed7963fec10b121bc5a
paizhang <zhangpai91@gmail.com>
2017-07-28 19:45:11 -0700

    Add initialize method

--- raceday/app/models/racer.rb
+++ raceday/app/models/racer.rb
@@ -1,5 +1,17 @@
 class Racer
 
+  attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
+
+  def initialize(params={})
+    @id=params[:_id].nil? ? params[:id] : params[:_id].to_s
+    @number=params[:number].to_i
+    @first_name=params[:first_name]
+    @last_name=params[:last_name]
+    @gender=params[:gender]
+    @group=params[:group]
+    @secs=params[:secs].to_i
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
