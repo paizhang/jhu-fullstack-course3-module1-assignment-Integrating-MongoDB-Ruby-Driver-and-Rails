8aab2c21e1f441eff82977eef3b5fdb2ad041790
paizhang <zhangpai91@gmail.com>
2017-07-28 19:29:07 -0700

    Add all class method

--- raceday/app/models/racer.rb
+++ raceday/app/models/racer.rb
@@ -8,6 +8,15 @@ class Racer
     self.mongo_client['racers']
   end
 
+  # Parameters prototype, sort, offset and limit are all optional.
+  def self.all(prototype={}, sort={:number=>1}, offset=0, limit=nil)
+    result=collection.find(prototype)
+               .sort(sort)
+               .skip(offset)
+    result=result.limit(limit) if !limit.nil?
+
+    return result
+  end
 
 
 end
